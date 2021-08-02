8b0b6f4db0431f2b611e3efc7b127dfda479aeca
Fulei Huang <fuleihuang@microsoft.com>
2021-08-02 19:58:39 +0100

    initial commit

--- .bash_history
+++
@@ -1,8 +0,0 @@
-ls 
-pwd
-code .
-ls models/
-nano data/nlu.yml 
-apt-get install nano
-sudo apt-get install nano
-exit
--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,4 @@
 cd app/
 # Start rasa server with nlu model
-rasa run --model models --enable-api --cors "*" --debug -p $PORT
+rasa run --model models --enable-api --cors "*" --debug \
+     -p $PORT
